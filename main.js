e49396c757a8260403a3888918d197e050e32c03
Jesus <jesus_xunxu@hotmail.com>
2015-11-10 14:17:49 +0100

    Added logo Disable zoom page in mobile devices Improve design with bootstrap and css

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ $( document ).ready(function() {
 
     function initialize() {
 
+        var pos;
         var map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 51.508742, lng: -0.120850},
             disableDefaultUI:true,
@@ -19,7 +20,7 @@ $( document ).ready(function() {
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
+                pos = {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                 };
@@ -32,18 +33,23 @@ $( document ).ready(function() {
                     tempMin = dailyWeather.list[0].temp.min;
                     tempMax = dailyWeather.list[0].temp.max;
                 });
-                var urlCurrentWeather = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&lang=es&units=metric&appid=2a1170ac8eb31642f058879ddd1c6933";
+                var urlCurrentWeather = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.lat + "&lon=" + pos.lng + "&units=metric&lang=es&appid=2a1170ac8eb31642f058879ddd1c6933";
+                console.log(urlCurrentWeather);
                 $.getJSON(urlCurrentWeather, function(currentWeather){
                     var urlIcon = "http://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png";
                     var htmlWeather = "<h3>" + currentWeather.name + ", " + currentWeather.sys.country + "</h3>" +
+                        "<div class='col-xs-6'>" +
+                            "<img src='" + urlIcon + "'>" +
+                            "<div>" + currentWeather.weather[0].main + "</div>" +
+                        "</div>" +
+                        "<div class='col-xs-6'>" +
+                            "<div>" + parseInt(currentWeather.main.temp) + "&#8451</div>" +
+                            "<div>" + parseInt(tempMax) + "&#8451</div>" +
+                            "<div>" + parseInt(tempMin) + "&#8451</div>" +
+                        "</div>" +
                         "<div>" +
-                        "<img src='" + urlIcon + "'>" +
-                        "<p>" + currentWeather.weather[0].main + "</p>" +
-                        "<p>" + parseInt(currentWeather.main.temp) + "&#8451</p>" +
-                        "<p>" + parseInt(tempMax) + "&#8451</p>" +
-                        "<p>" + parseInt(tempMin) + "&#8451</p>" +
-                        "<p>Humedad: " + currentWeather.main.humidity + "%</p>" +
-                        "<p>Viento: " + parseInt(currentWeather.wind.speed) + "kph</p>" +
+                        "<div>Humedad: " + currentWeather.main.humidity + "%</div>" +
+                        "<div>Viento: " + parseInt(currentWeather.wind.speed) + "kph</div>" +
                         "</div>";
                     infoWindow.setContent(htmlWeather);
                 });
@@ -58,10 +64,15 @@ $( document ).ready(function() {
             handleLocationError(false, infoWindow, map.getCenter());
         }
 
+        google.maps.event.addListener(map,'center_changed',function() {
+            window.setTimeout(function() {
+                map.panTo(pos);
+            },100);
+        });
+
     }
 
     initialize();
-
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
